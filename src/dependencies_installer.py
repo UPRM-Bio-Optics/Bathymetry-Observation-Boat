c3926115968d45f0c7d3324204692235bb302a40
AlonsoUriarte14 <diego.santiago6@upr.edu>
2022-02-17 20:36:05 -0400

    reformat code

--- src/dependencies_installer.py
+++ src/dependencies_installer.py
@@ -1,27 +1,24 @@
 import sys
 import subprocess
 
+
 # implement pip as a subprocess:
-def main(): 
-    
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-    'dronekit'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-    'pyserial'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-    'pynmea2'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-    'scipy'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-    'matplotlib'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-    'datetime'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-    'numpy'])
+def main():
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+                           'dronekit'])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+                           'pyserial'])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+                           'pynmea2'])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+                           'scipy'])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+                           'matplotlib'])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+                           'datetime'])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+                           'numpy'])
+
+
 if __name__ == '__main__':
     main()
-
-
-
-
-
