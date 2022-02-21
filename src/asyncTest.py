e1dfe6e3f33a310240c33cdc3964b07edc9b77e8
AcevedoC17 <carlos.acevedo17@upr.edu>
2022-02-20 22:01:25 -0400

    Added async concepts in main() should support running two couroutines in parallel.

--- src/asyncTest.py
+++ src/asyncTest.py
@@ -21,29 +21,80 @@ class Drone:
         self._vehicle_port = '/dev/USB0'  # dummy port; i forgor the port name
         self._echoounder_port = '/dev/ttyS0'  # same here lol
 
-        lat = np.array([])
-        lon = np.array([])
-        topo = np.array([])
-        today = date.today()
+        self.lat = np.array([])
+        self.lon = np.array([])
+        self.topo = np.array([])
+        self.today = date.today()
         # create txt file for error logs...
-        are_empty = lat.size == 0 or lon.size == 0 or topo.size == 0
+        are_empty = self.lat.size == 0 or self.lon.size == 0 or self.topo.size == 0
 
         self.csvfile = open(os.getcwd() + f'/src/Data/depth_data - ' +
-                       today.strftime("%b-%d-%Y") + '.csv')
-        writer = csv.writer(self.csvfile)
+                       self.today.strftime("%b-%d-%Y") + '.csv')
+        self.writer = csv.writer(self.csvfile)
         _header = ['Latitude', 'Longitude', 'Depth in Meters']
-        writer.writerow(_header)
+        self.writer.writerow(_header)
 
         self.vehicle = connect(self._vehicle_port, baud=115200, heartbeat_timeout=5)
-        cmds = self.vehicle.commands
-        cmds.download()
-        cmds.wait_ready()
-        missionlist = []
-        for cmd in cmds:
-            missionlist.append(cmd)
+        self.cmds = self.vehicle.commands
+        self.cmds.download()
+        self.cmds.wait_ready()
+        self.missionlist = []
+        for cmd in self.cmds:
+            self.missionlist.append(cmd)
 
 
-#####################################################################################################################################################################################
+#############################################################################################################################################
+    async def graph2d(self, lon, lat, topo):
+
+        resolution = 0.008333333333333333
+        # Determine the number of grid points in the x and y directions
+        nx = complex(0, (max(lon) - min(lon)) / resolution)
+        ny = complex(0, (max(lat) - min(lat)) / resolution)
+
+        # Build 2 grids: One with lats and the other with lons
+        grid_x, grid_y = np.mgrid[min(lon):max(lon):nx, min(lat):max(lat):ny]
+
+        # Interpolate topo into a grid (x by y dimesions)
+        grid_z = scipy.interpolate.griddata(
+            (lon, lat), topo, (grid_x, grid_y), method='cubic')
+
+        # plot
+        cs = plt.contourf(grid_x, grid_y, grid_z, cmap=cm.coolwarm)
+        plt.xlabel("Longitude", fontsize=15)
+        plt.ylabel("Latitude", fontsize=15)
+        plt.suptitle("Bathymetry Example", fontsize=18)
+        plt.colorbar()
+        # save Image and show it
+
+        plt.savefig(os.getcwd() + '/src/Graphs/TwoD map.png')
+        # plt.show()
+############################################################################################################################################################################
+    async def graph3d(self, lon, lat, topo):
+
+        resolution = 0.008333333333333333
+        # Determine the number of grid points in the x and y directions
+        nx = complex(0, (max(lon) - min(lon)) / resolution)
+        ny = complex(0, (max(lat) - min(lat)) / resolution)
+
+        # Build 2 grids: One with lats and the other with lons
+        grid_x, grid_y = np.mgrid[min(lon):max(lon):nx, min(lat):max(lat):ny]
+
+        # Interpolate topo into a grid (x by y dimesions)
+        grid_z = scipy.interpolate.griddata(
+            (lon, lat), topo, (grid_x, grid_y), method='cubic')
+
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        surf = ax.plot_surface(grid_x, grid_y, grid_z, cmap=cm.coolwarm)
+
+        plt.xlabel("Longitude")
+        plt.ylabel("Latitude")
+        plt.suptitle('Topograhy Surface Render', fontsize=18)
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+
+        plt.savefig(os.getcwd() + '/src/Graphs/ThreeD map.png')
+        # plt.show()
+
+    #####################################################################################################################################################################################
     async def run(self):
         _scannable = (self.vehicle.mode == 'AUTO' or self.vehicle.mode ==
                       'LOITER' or self.vehicle.mode == 'MANUAL') and self.cmds.next <= len(self.missionlist)
@@ -73,8 +124,23 @@ class Drone:
                     sleep(1)
 
         self.csvfile.close()
-        graph2d(lon, lat, topo)
-        graph3d(lon, lat, topo)
+        await self.graph2d(self.lon, self.lat, self.topo)
+        await self.graph3d(self.lon, self.lat, self.topo)
 
 
 async def main():
+    loop = asyncio.get_event_loop() # this handles loop
+    drone = Drone()
+    try:
+        asyncio.ensure_future(drone.run()) # We ensure the future for all the tasks we want.
+        loop.run_forever() # Loop runs forever
+    except KeyboardInterrupt: # We can interrupt the program with CTRL^C etc
+        pass
+    finally: # Close the loop.
+        print("Goodbye.")
+        loop.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
