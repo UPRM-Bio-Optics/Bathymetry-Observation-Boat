ba3ce30ea5eb3a26ef6268528c6327c9bbb5b3fd
AlonsoUriarte14 <diego.santiago6@upr.edu>
2022-02-26 11:36:32 -0400

    refactor server.py

--- server.py
+++ server.py
@@ -1,27 +1,36 @@
 import socket
+import string
 import time
-while True:
-  try:
+
+
+def sock() -> str:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print ("Socket successfully created")
+    print("Socket successfully created")
     port = 3389
     s.bind(('', port))
-    print ("socket binded to %s" %(port))
+    print("socket binded to %s" % (port))
     s.listen(5)
-    print ("socket is listening")
+    print("socket is listening")
     c, addr = s.accept()
-    print ('Got connection from', addr )
+    print('Got connection from', addr)
     c.send('Thank you for connecting'.encode())
     message = c.recv(1024).decode()
-    f = open("latestIP.txt", "w")
-    f.write(message)
-    f.close()
-    print("Closing")
     c.close()
-    time.sleep(100)
-  except KeyboardInterrupt:
-    break
-  except:
-    continue
+    return message
 
+if __name__ == '__main__':
+  
+    while True:
+        try:
+            message = sock()
+        except Exception as e:
+            print(f'Exception Occurred: {e.message}')
+            print('Retrying Connection...')
+            continue
 
+        f = open("latestIP.txt", "w")
+        f.write(message)
+        f.close()
+        print("Closing")
+        time.sleep(100)
+    
