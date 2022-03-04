13de7f48ad239065b4695c2d875f102bc9f7c9e1
AcevedoC17 <77506847+AcevedoC17@users.noreply.github.com>
2022-03-03 21:34:58 -0400

    Funny wacky debug snippet works

--- src/debug_snippet.py
+++ src/debug_snippet.py
@@ -1,20 +1,18 @@
 import sys
 import glob
-from configparser import ParsingError
 import serial
 import pynmea2
+import numpy as np
+import time
+import csv
+import os
+from datetime import date
 
-'''
-TODO
-- determine ports
-- test readline / test different bauds
-- test pixhawk same time 
-- implement whatever works here to test 
-'''
+
 
 def serial_ports():
     """ Lists serial port names
-
+        
         :raises EnvironmentError:
             On unsupported or unknown platforms
         :returns:
@@ -40,39 +38,50 @@ def serial_ports():
             pass
     return result
 
-def readParser(ser: serial.Serial) -> str:
-    
-    line  = ser.read(100)
-    line = line[line.find('$'): line.find('\n')] # -1 maybe
-    return line    
 
-def main() -> None:
-    
-    ports = serial_ports()
-    print(ports)
-    with serial.Serial('/dev/USB0', baudrate=4800, timeout=1) as ser:
+if __name__ == '__main__':
+    lat = np.array([])
+    lon = np.array([])
+    topo = np.array([])
+    today = date.today()
+    csvfilePath = 'Data/depth_data/' + today.strftime("%b-%d-%Y") + '.csv'
+    csvfile = open(csvfilePath, 'w')
+        
+    #csvfile = open(os.getcwd() + f'/src/Data/depth_data/' +
+                   #today.strftime("%b-%d-%Y") + '.csv')
+    writer = csv.writer(csvfile)
+    _header = ['Latitude', 'Longitude', 'Depth in Meters']
+    writer.writerow(_header)
+    with serial.Serial('/dev/ttyUSB0', baudrate=4800, timeout=1) as ser:
         for i in range(15):
 
             line = ser.readline().decode('ascii', 'ignore')
-            #line = readParser(ser)
+            row = [None, None, 1]
             try:
                 obj = pynmea2.parse(line)
-            except ParsingError:
-                print(f'Could Not Parse Data: {line} ')
+            except:
+                print('Could Not Parse Data: ', line) # print(f'was not working.)
                 print(line.startswith('$'))
                 continue
 
             if obj.sentence_type == 'DPT':
-                print(
-                    f'Some depth data for you, NMEA GOD: DEPTH = {obj.depth} meters')
+                print('Some depth data for you, NMEA GOD: DEPTH = ', obj.depth) #print(f'Some depth data for you, NMEA GOD: DEPTH = {obj.depth} meters')
+                row[2] = 1
             elif obj.sentence_type == 'GGA':
-                print(
-                    f'Some coordinates for you, NMEA GOD: LAT = {obj.latitude}, LON = {obj.longitude} ')
+                print('Some coordinates for you, NMEA GOD: LAT = ', obj.latitude)
+                np.append(lat, obj.latitude)
+                print('Some coordinates for you, NMEA GOD: LON = ', obj.longitude) #print(f'Some coordinates for you, NMEA GOD: LAT = {obj.latitude}, LON = {obj.longitude} ')
+                np.append(lon, obj.lon)
+                row[0] = obj.latitude
+                row[1] = obj.longitude
+            if all(row):
+                writer.writerow(row)
+                #time.sleep(1)
             else:
-                print(f'Some other NMEA sentence of type: {obj.sentence_type} ')
-
-    print('DONE!!!!')
-
-
-if __name__ == '__main__':
-    main()
+                print('Some other NMEA sentence of type: ', obj.sentence_type)
+            
+            #time.sleep(5)
+        temp = [1,2,3] #temp data
+        writer.writerow(temp)
+        csvfile.close()
+        print('DONE!!!!')
