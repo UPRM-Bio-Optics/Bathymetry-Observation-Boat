694ad738d41626291c9acd20548def724d2d26ae
Diego A. Santiago Uriarte <90219728+AlonsoUriarte14@users.noreply.github.com>
2022-03-22 13:30:22 -0400

    Update asyncTest.py

--- src/asyncTest.py
+++ src/asyncTest.py
@@ -1,6 +1,5 @@
 import asyncio
 from configparser import ParsingError
-import numpy
 import serial
 import pynmea2
 import os
@@ -17,24 +16,31 @@ from time import sleep
 class Drone:
     def __init__(self):
         # do whatever on startup, could be setting up files, directories, maybe the data structures to use.
-        self._vehicle_port = '/dev/USB0'  # dummy port; i forgor the port name
-        self._echoounder_port = '/dev/ttyS0'  # same here lol
+        self._vehicle_port = '/dev/ttyACM0'  # dummy port; i forgor the port name
+        self._echosounder_port = '/dev/ttyUSB0'  # same here lol
         self.dataDict = {'lat': np.array([]), 'lon': np.array([]), 'topo': np.array([])}
-        self.today = date.today()
+        today = date.today().strftime("%b-%d-%Y")
         # create txt file for error logs...
         are_empty = self.dataDict['lat'].size == 0 or self.dataDict['lon'].size == 0 or self.dataDict['topo'].size == 0
 
-        self.csvfile = open(os.getcwd() + f'/src/Data/depth_data - ' +
-                            self.today.strftime("%b-%d-%Y") + '.csv')
+        self.csvfile = open(os.getcwd() + '/Data/depth_data/' + today + '.csv', 'w')
         self.writer = csv.writer(self.csvfile)
         _header = ['Latitude', 'Longitude', 'Depth in Meters']
         self.writer.writerow(_header)
 
-        self.vehicle = connect(self._vehicle_port, baud=115200, heartbeat_timeout=5)
-        self.cmds = self.vehicle.commands
-        self.cmds.download()
-        self.cmds.wait_ready()
-        self.missionlist = []
+        while True:
+            try:
+                self.vehicle = connect(self._vehicle_port, baud=115200, heartbeat_timeout=5)
+                self.cmds = self.vehicle.commands
+                self.cmds.download()
+                self.cmds.wait_ready()
+                self.missionlist = []
+                break
+            
+            except Exception as e:
+                print('Could not connect to Pixhawk')
+                print (e)
+                continue
         for cmd in self.cmds:
             self.missionlist.append(cmd)
 
@@ -93,15 +99,17 @@ class Drone:
     #####################################################################################################################################################################################
     async def run(self):
         _scannable = (self.vehicle.mode == 'AUTO' or self.vehicle.mode ==
-                      'LOITER' or self.vehicle.mode == 'MANUAL') and self.cmds.next <= len(self.missionlist)
-
-        with serial.Serial(self._echoounder_port, baudrate=4800, timeout=2) as ser:
-            while _scannable:
+                      'LOITER' or self.vehicle.mode == 'MANUAL') or self.cmds.next <= len(self.missionlist)
+        print("About to enter loop.")
+        with serial.Serial(self._echosounder_port, baudrate=4800, timeout=2) as ser:
+            rows = 0
+            for i in range(10): # for testing without pixhawk, uncomment above line to test with pixhawk.
+            #while _scannable:
                 try:
                     line = ser.readline().decode('ascii', 'ignore')
                     nmea_object = pynmea2.parse(line)
-                    row = [None, None, None]
-                except ParsingError:
+                    row = [None, None, 1]
+                except Exception:
                     continue
 
                 if nmea_object.sentence_type == 'DPT':
@@ -109,6 +117,7 @@ class Drone:
                     row[2] = nmea_object.depth
 
                 elif nmea_object.sentence_type == 'GGA':
+                    print("Appending GPS data: ", nmea_object.latitude, nmea_object.longitude)
                     np.append(self.dataDict['lat'], nmea_object.latitude)
                     np.append(self.dataDict['lon'], nmea_object.longitude)
 
@@ -116,26 +125,49 @@ class Drone:
                     row[1] = nmea_object.longitude
 
                 if all(row):
+                    print('Adding row to csv')
                     self.writer.writerow(row)
-                    await asyncio.sleep(1)
-
+                    rows+=1
+                _scannable = self.vehicle.armed
+                print("Vehicle armed status: ", _scannable)
+                await asyncio.sleep(2)
+                
+        print("Done with mission.")
+         # try:
+                
+    #     graph2d(lon, lat, topo)
+    #     graph3d(lon, lat, topo)
+    # except Exception as e:
+         
+    #     print(' AT least you tried graphs :l')
+    #     row = ['could not graph', 'error', e]
+    #     writer.writerow(row)
         self.csvfile.close()
-        await self.graph2d(self.dataDict['lon'], self.dataDict['lat'], self.dataDict['topo'])
-        await self.graph3d(self.dataDict['lon'], self.dataDict['lat'], self.dataDict['topo'])
-
+        # await self.graph2d(self.dataDict['lon'], self.dataDict['lat'], self.dataDict['topo'])
+        # await self.graph3d(self.dataDict['lon'], self.dataDict['lat'], self.dataDict['topo'])
+class dummyObject:
+    async def printThing(self):
+        while True:
+            print("That other loop is sleeping :O")
+            await asyncio.sleep(2)
+        
+    
 
 async def main():
     loop = asyncio.get_event_loop()  # this handles loop
     drone = Drone()
+    dummy = dummyObject()
     try:
-        asyncio.ensure_future(drone.run())  # We ensure the future for all the tasks we want.
-        loop.run_forever()  # Loop runs forever
+        # await asyncio.ensure_future(drone.run())
+        # await asyncio.ensure_future(dummy.printThing())# We ensure the future for all the tasks we want.
+        # ^^^^^^^^^^^^^^ That ensure future does NOT work.
+        await asyncio.gather(drone.run(), dummy.printThing()) #Gather works, just add the loops here and the couroutines will work when the others are sleeping.
+        #loop.run_forever()  # Loop runs forever
     except KeyboardInterrupt:  # We can interrupt the program with CTRL^C etc
         pass
-    finally:  # Close the loop.
-        print("Goodbye.")
-        loop.close()
 
 
 if __name__ == '__main__':
     asyncio.run(main())
+
+  
