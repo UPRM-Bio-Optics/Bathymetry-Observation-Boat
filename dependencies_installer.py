179744222231bce919a658b7ff05b5f2dc51b35f
AlonsoUriarte14 <diego.santiagot6@upr.edu>
2023-05-05 16:33:30 -0400

    changed order of installs correct dependencies  error

--- dependencies_installer.py
+++ dependencies_installer.py
@@ -8,37 +8,24 @@ def main():
     """
     Run This Python Script install all the dependencies on any OS
     """
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'dronekit'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'pyserial'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'pynmea2'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'scipy==1.8.1'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'matplotlib'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'datetime'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'numpy'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'requests'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'pandas'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'plotly'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'requests'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'selenium'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'pysimplegui'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           'seabreeze[pyseabreeze]'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-                           '-U', 'kaleido'])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "dronekit"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pyserial"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pynmea2"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "matplotlib"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "datetime"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "scipy==1.8.1"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pandas"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "plotly"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "selenium"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pysimplegui"])
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "seabreeze[pyseabreeze]"]
+    )
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-U", "kaleido"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
