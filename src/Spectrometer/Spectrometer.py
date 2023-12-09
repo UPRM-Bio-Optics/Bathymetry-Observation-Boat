530703434112085310e89cbf3942ad4b3b0bcf29
AlonsoUriarte14 <diego.santiago6@upr.edu>
2023-12-09 18:25:29 -0400

    - included both spectrometer in same class - calculates reflecctance - stores buffer spectrum in dicitonary

--- src/Spectrometer/Spectrometer.py
+++ src/Spectrometer/Spectrometer.py
@@ -12,15 +12,16 @@ seabreeze.use("pyseabreeze")
 from seabreeze.spectrometers import Spectrometer
 
 
-class SpectrometerWrapper:
-    wavelengthsBuffer = []
-    intensitiesBuffer = []
+class SpectrometerSystem:
+    #wavelength key, intensity value
+    skySpectrum = {}
+    #wavelength key, intensity value
+    seaSpectrum = {}
+    #wavelength key, reflectance value
+    reflectance = {}
+    
 
-    device = None
-    outputFile = None
-    writer = None
-
-    def __init__(self, serialNumber: int = None) -> None:
+    def __init__(self, skySerialNumber: int = None, seaSerialNumber: int = None) -> None:
         """
         Initialize spectrometer object class; This is a wrapper to the
         PySeaBreeze Spectrometer class to include several methods such as noise reduction
@@ -29,12 +30,20 @@ class SpectrometerWrapper:
         Args:
             serialNumber (int, optional): if initializing spectrometer from serial number instead of through from first availvable . Defaults to None.
         """
-        if serialNumber:
-            self.device = Spectrometer.from_serial_number(serialNumber)
+        if skySerialNumber:
+            self.device = Spectrometer.from_serial_number(skySerialNumber)
         else:
-            self.device = Spectrometer.from_first_available()
+            self.skySpectrometer = Spectrometer.from_first_available()
+        
+        if seaSerialNumber:
+            self.device = Spectrometer.from_serial_number(seaSerialNumber)
+        else:
+            self.seaSpectrometer = Spectrometer.from_first_available()
 
-        self.device.integration_time_micros(100000)
+
+        self.skySpectrometer.integration_time_micros(100000)
+        self.seaSpectrometer.integration_time_micros(100000)
+        
 
         self.today = datetime.now().strftime("%m-%d-%Y")
         self.outputFile = open(
@@ -44,7 +53,7 @@ class SpectrometerWrapper:
         _header = ["Wavelengths (nm)", "Intensities (a.u)"]
         self.writer.writerow(_header)
 
-    def reduceNoise(self) -> None:
+    def reduceNoise(self, wavelengthsBuffer, intensitiesBuffer) -> None:
         """Removes duplicate intensities values from wavelengths list and condenses them to a single entry with
         the average of the associated intensities for a given wavelength;
         Note: Uses current wavelengths and intensities buffer values;
@@ -53,24 +62,29 @@ class SpectrometerWrapper:
         result: dict = {}
         duplicateSum = 0
         duplicateCount = 0
-        for i in range(len(self.wavelengthsBuffer) - 1):
+        for i in range(len(wavelengthsBuffer) - 1):
             duplicateCount += 1
-            duplicateSum += self.intensitiesBuffer[i]
-            if self.wavelengthsBuffer[i] != self.wavelengthsBuffer[i + 1]:
-                result[self.wavelengthsBuffer[i]] += duplicateSum / duplicateCount
+            duplicateSum += intensitiesBuffer[i]
+            if wavelengthsBuffer[i] != self.wavelengthsBuffer[i + 1]:
+                result[wavelengthsBuffer[i]] += duplicateSum / duplicateCount
 
                 duplicateCount = 0
                 duplicateSum = 0
 
-        self.wavelengthsBuffer, self.intensitiesBuffer = zip(*result.items())
+        return result
 
     def fillBuffer(self) -> None:
         """
-        Updates buffer with new sample
+        Updates spectrum buffer with new sample
 
         """
-        self.wavelengthsBuffer, self.intensitiesBuffer = self.device.spectrum()
-        self.reduceNoise()
+        wavelengthsBuffer, intensitiesBuffer = self.skySpectrometer.spectrum()
+        self.skySpectrum = self.reduceNoise(wavelengthsBuffer, intensitiesBuffer)
+        
+        wavelengthsBuffer, intensitiesBuffer = self.seaSpectrometer.spectrum()
+        self.seaSpectrum = self.reduceNoise(wavelengthsBuffer, intensitiesBuffer)
+        
+        
 
     def plotBuffer(self) -> None:
         """
@@ -91,18 +105,37 @@ class SpectrometerWrapper:
         fig.write_image(filename)
         # plt.savefig(os.getcwd() + "/Data/Spectrometer/plots/" + today + ".png")
 
-    @staticmethod
-    def plot(csvpath: str):
-        df = pd.read_csv(csvpath)
-        fig = px.line(
-            data_frame=df,
-            labels={"x": "Wavelength (nm)", "y": "Intensity (a.u)"},
-            title="Intensity vs Wavelength",
-        )
+
+    def calculateReflectance(self) -> None:
+        
+        """
+        Calculates reflectance based on the intensity data measured by each spectrometer. divinging the below intensity over the light
+        """
+        
+        for wavelength in self.skySpectrum.keys():
+            
+            self.reflectance[wavelength] = self.seaSpectrum[wavelength] / self.skySpectrum[wavelength]
+        
+        
+
+
+def plot(csvpath: str):
+    """
+    Plot Reflectance vs wavelength using data from csv
+
+    Args:
+        csvpath (str): file path to csv
+    """
+    df = pd.read_csv(csvpath)
+    fig = px.line(
+        data_frame=df,
+        labels={"x": "Wavelength (nm)", "y": "Intensity (a.u)"},
+        title="Intensity vs Wavelength",
+    )
 
 
 def spectro():
-    spec = SpectrometerWrapper()
+    spec = SpectrometerSystem()
     spec.fillBuffer()
     spec.plotBuffer()
 
